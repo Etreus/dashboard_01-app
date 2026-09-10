d079a6ce38b8766eaaca7ee75556df00ea58cce5
Etreus <jaimejalife@gmail.com>
2026-09-09 23:25:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
-import os
-os.system("pip install matplotlib requests pandas seaborn plotly")
+import subprocess
+import sys
+
+# Ejecuta la instalación usando el módulo recomendado y el ejecutable actual de Python
+subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib", "requests", "pandas", "seaborn", "plotly"])
 
 import streamlit as st
 import requests
